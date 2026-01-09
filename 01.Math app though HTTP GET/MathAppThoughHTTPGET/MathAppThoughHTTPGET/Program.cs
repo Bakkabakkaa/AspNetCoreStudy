6e6e974a7466862282b90f21769a05bba41b2e69
Art'm <kasko.artem45@gmail.com>
2026-01-10 02:31:21 +0300

    add reading of the first number

--- 01.Math app though HTTP GET/MathAppThoughHTTPGET/MathAppThoughHTTPGET/Program.cs
+++ 01.Math app though HTTP GET/MathAppThoughHTTPGET/MathAppThoughHTTPGET/Program.cs
@@ -3,4 +3,28 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+app.Run(async (HttpContext context)=>
+{
+    if (context.Request.Method == "GET" && context.Request.Path == "/")
+    {
+        var firstNumber = 0;
+        var secondNumber = 0;
+        var operation = "";
+        float result = 0;
+
+        if (context.Request.Query.ContainsKey("firstNumber"))
+        {
+            string stringFirstNumber = context.Request.Query["firstNumber"][0];
+            if (!string.IsNullOrEmpty(stringFirstNumber))
+            {
+                firstNumber = Convert.ToInt32(stringFirstNumber);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Неверный ввод первого числа");
+            }
+        }
+
+    }
+});
