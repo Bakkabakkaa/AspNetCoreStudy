36d420b03943412b53e5b84c63febf17f3159278
Art'm <kasko.artem45@gmail.com>
2026-01-10 03:52:01 +0300

    add reading of the second number and operations, as well as output of the result in response

--- 01.Math app though HTTP GET/MathAppThoughHTTPGET/MathAppThoughHTTPGET/Program.cs
+++ 01.Math app though HTTP GET/MathAppThoughHTTPGET/MathAppThoughHTTPGET/Program.cs
@@ -1,20 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 app.Run(async (HttpContext context)=>
 {
     if (context.Request.Method == "GET" && context.Request.Path == "/")
     {
         var firstNumber = 0;
         var secondNumber = 0;
-        var operation = "";
-        float result = 0;
+        string? operation = null;
+        float? result = null;
 
         if (context.Request.Query.ContainsKey("firstNumber"))
         {
-            string stringFirstNumber = context.Request.Query["firstNumber"][0];
+            var stringFirstNumber = context.Request.Query["firstNumber"][0];
+            
             if (!string.IsNullOrEmpty(stringFirstNumber))
             {
                 firstNumber = Convert.ToInt32(stringFirstNumber);
@@ -25,6 +24,78 @@ app.Run(async (HttpContext context)=>
                 await context.Response.WriteAsync("Неверный ввод первого числа");
             }
         }
+        else
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("Неверный ввод первого числа");
+        }
 
+        if (context.Request.Query.ContainsKey("secondNumber"))
+        {
+            var stringSecondNumber = context.Request.Query["secondNumber"][0];
+            
+            if (!string.IsNullOrEmpty(stringSecondNumber))
+            {
+                secondNumber = Convert.ToInt32(stringSecondNumber);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Неверный ввод второго числа");
+            }
+        }
+        else
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("Неверный ввод второго числа");
+        }
+
+        if (context.Request.Query.ContainsKey("operation"))
+        {
+            operation = context.Request.Query["operation"][0];
+
+            if (!string.IsNullOrEmpty(operation))
+            {
+                switch (operation)
+                {
+                    case "add":
+                        result = firstNumber + secondNumber;
+                        break;
+                    case "multiply":
+                        result = firstNumber * secondNumber;
+                        break;
+                    case "subtract":
+                        result = firstNumber - secondNumber;
+                        break;
+                    case "divide":
+                        result = secondNumber == 0 ? 0 : firstNumber / secondNumber;
+                        break;
+                    case "mod":
+                        result = secondNumber == 0 ? 0 : firstNumber % secondNumber;
+                        break;
+                    default:
+                        context.Response.StatusCode = 400;
+                        await context.Response.WriteAsync("Неверный ввод операции над числами");
+                        return;
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Неверный ввод операции над числами");
+            }
+        }
+        else
+        {
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("Неверный ввод операции над числами");
+        }
+
+        if (result.HasValue)
+        {
+            await context.Response.WriteAsync(result.ToString());
+        }
     }
 });
+
+app.Run();
