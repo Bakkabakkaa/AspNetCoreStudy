f35a17a061bbc1b0010b233815aa381e154f8b65
Art'm <kasko.artem45@gmail.com>
2026-01-13 21:10:22 +0300

    add WelcomeMessage endpoint

--- 04. Bank app using Controllers/BankAppUsingControllers/BankAppUsingControllers/Controllers/BankAccountsController.cs
+++ 04. Bank app using Controllers/BankAppUsingControllers/BankAppUsingControllers/Controllers/BankAccountsController.cs
@@ -7,6 +7,11 @@ public class BankAccountsController : Controller
     private int _accountNumber = 1001;
     private string _accountHolderName = "Example Name";
     private int _currentBalance = 5000;
-    
-    
+
+    [HttpGet]
+    [Route("/")]
+    public IActionResult WelcomeMessage()
+    {
+        return Content("Welcome to the Best Bank");
+    }
 }
