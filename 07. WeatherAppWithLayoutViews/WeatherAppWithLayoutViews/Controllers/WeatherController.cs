689ec4b2ed75d420fa7e909b1e0427854e68887b
Art'm <kasko.artem45@gmail.com>
2026-01-19 00:53:18 +0300

    add list of city

---
+++ 07. WeatherAppWithLayoutViews/WeatherAppWithLayoutViews/Controllers/WeatherController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using WeatherAppWithLayoutViews.Models;
+
+namespace WeatherAppWithLayoutViews.Controllers;
+
+public class WeatherController : Controller
+{
+    private List<CityWeather> cities = new List<CityWeather>()
+    {
+        new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureFahrenheit = 33 },
+        new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureFahrenheit = 60 },
+        new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureFahrenheit = 82 }
+    };
+}
