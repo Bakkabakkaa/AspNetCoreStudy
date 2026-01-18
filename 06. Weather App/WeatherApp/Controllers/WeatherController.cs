0cc07803a7a9a9e456e2fe98f2628e19bdd14f2d
Art'm <kasko.artem45@gmail.com>
2026-01-18 03:53:55 +0300

    add _ViewImports.cshtml, MainScreen.cshtml and AllCities endpoint

--- 06. Weather App/WeatherApp/Controllers/WeatherController.cs
+++ 06. Weather App/WeatherApp/Controllers/WeatherController.cs
@@ -11,6 +11,12 @@ public class WeatherController : Controller
         new CityWeather(){ CityUniqueCode = "NYC", CityName = "London", DateAndTime = new DateTime(2030, 1, 1, 3, 0, 0), TemperatureFahrenheit = 60},
         new CityWeather(){CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = new DateTime(2030, 1, 1, 9, 0, 0), TemperatureFahrenheit = 82}
     };
-    
+
+    [HttpGet]
+    [Route("/")]
+    public IActionResult AllCities()
+    {
+        return View("MainScreen", cities);
+    }
     
 }
