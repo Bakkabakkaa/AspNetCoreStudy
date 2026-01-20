e045ce5db339f49a4044b3559b38258977c704a0
Art'm <kasko.artem45@gmail.com>
2026-01-21 01:16:11 +0300

    add WeatherController.cs and Index, City endpoints

---
+++ 08. Weather App With Partial Views/WeatherAppWithPartialViews/Controllers/WeatherController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using WeatherAppWithPartialViews.Models;
+
+namespace WeatherAppWithPartialViews.Controllers;
+
+public class WeatherController : Controller
+{
+    private List<CityWeather> cities = new List<CityWeather>() {
+        new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureFahrenheit = 33 },
+
+        new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureFahrenheit = 60 },
+
+        new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureFahrenheit = 82 }
+    };
+    
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View(cities);
+    }
+
+    [Route("/weather/{cityCode?}")]
+    public IActionResult City(string cityCode)
+    {
+        var city = cities.FirstOrDefault(currentCity => currentCity.CityUniqueCode == cityCode);
+
+        return View(city);
+    }
+}
