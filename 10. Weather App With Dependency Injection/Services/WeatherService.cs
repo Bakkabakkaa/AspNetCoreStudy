780583e6e2ff670441e0c7e92789dba99c8cc2d5
Art'm <kasko.artem45@gmail.com>
2026-01-23 03:46:24 +0300

    add a class library based on the model

---
+++ 10. Weather App With Dependency Injection/Services/WeatherService.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Models;
+
+namespace Services;
+
+public class WeatherService
+{
+    private readonly List<CityWeather> _cities;
+
+    public WeatherService()
+    {
+        _cities = new List<CityWeather>() {
+            new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"), TemperatureFahrenheit = 33 },
+
+            new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"), TemperatureFahrenheit = 60 },
+
+            new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"), TemperatureFahrenheit = 82 }
+        };
+    }
+
+    public List<CityWeather> GetWeatherDetails()
+    {
+        return _cities;
+    }
+
+    public CityWeather? GetWeatherByCityCode(string cityCode)
+    {
+        var city = _cities.FirstOrDefault(currentCity => currentCity.CityUniqueCode == cityCode);
+        return city;
+    }
+}
--- 10. Weather App With Dependency Injection/WeatherAppWithDependencyInjection/Models/CityWeather.cs
+++
@@ -1,9 +0,0 @@
-namespace WeatherAppWithDependencyInjection.Models;
-
-public class CityWeather
-{
-    public string CityUniqueCode { get; set; }
-    public string CityName { get; set; }
-    public string DateAndTime { get; set; }
-    public string TemperatureFahrenheit { get; set; }
-}
