5c85dc40e33130fd7fef31438e0bae7137c7ab2c
Art'm <kasko.artem45@gmail.com>
2026-01-24 00:02:46 +0300

    Add a method for getting the CSS class in the view component

---
+++ 10. Weather App With Dependency Injection/WeatherAppWithDependencyInjection/ViewComponents/CityViewComponents.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WeatherAppWithDependencyInjection.ViewComponents;
+
+public class CityViewComponents : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
+    {
+        ViewBag.CityCssClass = GetCssClassByTemperature(city.TemperatureFahrenheit);
+
+        return View(city);
+    }
+
+    private string GetCssClassByTemperature(int temperature)
+    {
+        switch (temperature)
+        {
+            case < 44:
+                return "blue-back";
+            case < 75:
+                return "green-back";
+            default:
+                return "orange-back";
+        }
+    }
+}
