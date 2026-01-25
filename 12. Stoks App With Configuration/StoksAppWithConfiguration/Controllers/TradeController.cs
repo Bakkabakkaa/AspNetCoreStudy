767b6d0f6c0b634845ebf9f5d92d308fc8c77baf
Art'm <kasko.artem45@gmail.com>
2026-01-25 23:03:13 +0300

    Add logic for receiving a response from FinHub to the controller

--- 12. Stoks App With Configuration/StoksAppWithConfiguration/Controllers/TradeController.cs
+++ 12. Stoks App With Configuration/StoksAppWithConfiguration/Controllers/TradeController.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
 
 namespace StoksAppWithConfiguration.Controllers;
 
 public class TradeController : Controller
 {
-    [Route("/")]
-    public IActionResult Index()
+    private readonly IFinnhubService _finnhubService;
+    public TradeController(IFinnhubService finnhubService)
     {
+        _finnhubService = finnhubService;
+    }
+    [Route("/")]
+    public async Task<IActionResult> Index()
+    {
+        Dictionary<string, object>? responseDictionary = await _finnhubService.GetStockPriceQuote("MSFT");
         return View();
     }
 }
