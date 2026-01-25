978a1edffc076c6a189f944e765caa9815fceaa2
Art'm <kasko.artem45@gmail.com>
2026-01-25 20:57:29 +0300

    create FinhubServise.cs

---
+++ 12. Stoks App With Configuration/Services/FinhubService.cs
@@ -0,0 +1,11 @@
+﻿namespace Services;
+
+public class FinhubService
+{
+   private readonly IHttpClientFactory _httpClientFactory;
+
+   public FinhubService(IHttpClientFactory httpClientFactory)
+   {
+      _httpClientFactory = httpClientFactory;
+   }
+}
