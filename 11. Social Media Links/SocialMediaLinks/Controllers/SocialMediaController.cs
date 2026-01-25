daf1e14c5b3e6ced0cef259e246eb1c5af8ed01c
Art'm <kasko.artem45@gmail.com>
2026-01-25 17:58:22 +0300

    add socialmedia to viewbag

--- 11. Social Media Links/SocialMediaLinks/Controllers/SocialMediaController.cs
+++ 11. Social Media Links/SocialMediaLinks/Controllers/SocialMediaController.cs
@@ -1,14 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace SocialMediaLinks.Controllers;
 
 public class SocialMediaController : Controller
 {
+    private readonly SocialMediaLinksOptions _socialMediaLinksOptions;
+    
+    public SocialMediaController(IOptions<SocialMediaLinksOptions> socialMediaLinksOptions)
+    {
+        _socialMediaLinksOptions = socialMediaLinksOptions.Value;
+    }
+    
     [HttpGet]
     [Route("/")]
     public IActionResult Index()
     {
-        
+        ViewBag.SocialMedia = _socialMediaLinksOptions;
         return View();
     }
 }
