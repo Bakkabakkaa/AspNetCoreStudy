b4746ed1d687ebf83495184e927fe9adef10d12d
Art'm <kasko.artem45@gmail.com>
2026-01-25 17:39:35 +0300

    configure SocialMediaLinks options from configuration

--- 11. Social Media Links/SocialMediaLinks/Program.cs
+++ 11. Social Media Links/SocialMediaLinks/Program.cs
@@ -1,5 +1,8 @@
+using SocialMediaLinks;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<SocialMediaLinksOptions>(builder.Configuration.GetSection("SocialMediaLinks"));
 var app = builder.Build();
 
 app.UseStaticFiles();
