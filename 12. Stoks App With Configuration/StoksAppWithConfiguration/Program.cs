b4fc9ebe601742b9d5b5de707018ebbbf6cd0fda
Art'm <kasko.artem45@gmail.com>
2026-01-26 18:13:31 +0300

    configure Finnhub service and TradingOptions

--- 12. Stoks App With Configuration/StoksAppWithConfiguration/Program.cs
+++ 12. Stoks App With Configuration/StoksAppWithConfiguration/Program.cs
@@ -1,5 +1,11 @@
+using ServiceContracts;
+using Services;
+using StoksAppWithConfiguration;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<TradingOptions>(builder.Configuration.GetSection("TradingOptions"));
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IFinnhubService, FinnhubService>();
 builder.Services.AddHttpClient();
 var app = builder.Build();
 
