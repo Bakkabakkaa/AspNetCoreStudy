b4b2c2f632339abe0498120ea14412e0a1110d2d
Art'm <kasko.artem45@gmail.com>
2026-03-06 02:53:04 +0300

    Add migration and update database. Add required name

--- 14. Web API Project/CitiesManager/Models/City.cs
+++ 14. Web API Project/CitiesManager/Models/City.cs
@@ -7,6 +7,7 @@ public class City
     [Key]
     public Guid CityID { get; set; }
     
+    [Required(ErrorMessage = "City Name can't be blank")]
     public string? CityName { get; set; }
     
     
