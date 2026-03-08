78c41b53d2b0ca2e44b7c9602ff1d9d8140ccfbd
Art'm <kasko.artem45@gmail.com>
2026-03-08 15:08:34 +0300

    Add documentation comments

--- 14. Web API Project/CitiesManager/Program.cs
+++ 14. Web API Project/CitiesManager/Program.cs
@@ -13,7 +13,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer(); // Generates description for all endpoints
-builder.Services.AddSwaggerGen(); // Generates OpenAPI specification
+builder.Services.AddSwaggerGen( options => 
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "api.xml"))); // Generates OpenAPI specification
 
 var app = builder.Build();
 
