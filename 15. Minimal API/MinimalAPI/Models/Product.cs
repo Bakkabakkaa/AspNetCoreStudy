6910d3fa25ffc0d15eaab3af12e5dacf284f1af7
Art'm <kasko.artem45@gmail.com>
2026-03-11 20:50:33 +0300

    Create Product.cs model

---
+++ 15. Minimal API/MinimalAPI/Models/Product.cs
@@ -0,0 +1,7 @@
+namespace MinimalAPI.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string? ProductName { get; set; }
+}
--- 15. Minimal API/MinimalAPI/Program.cs
+++ 15. Minimal API/MinimalAPI/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Get
+app.MapGet("/", async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("GET - Hello World");
+});
+
+app.MapPost("/", async (HttpContext context) =>
+{
+    await context.Response.WriteAsync("GET - Hello World");
+});
 
 app.Run();
