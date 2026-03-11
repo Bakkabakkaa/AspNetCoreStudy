f15a6539455bbd584d3426cfc63af75a1d5e6e4e
Art'm <kasko.artem45@gmail.com>
2026-03-11 20:55:57 +0300

    Override ToString product method

--- 15. Minimal API/MinimalAPI/Models/Product.cs
+++ 15. Minimal API/MinimalAPI/Models/Product.cs
@@ -4,4 +4,9 @@ public class Product
 {
     public int Id { get; set; }
     public string? ProductName { get; set; }
+
+    public override string ToString()
+    {
+        return $"Product ID: {Id}, Product Name: {ProductName}";
+    }
 }
