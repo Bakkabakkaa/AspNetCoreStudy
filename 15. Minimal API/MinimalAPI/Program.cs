f969c1eaf1a54c75cdb4947aff023c326f166e3c
Art'm <kasko.artem45@gmail.com>
2026-03-11 22:35:01 +0300

    Route Parameters

--- 15. Minimal API/MinimalAPI/Program.cs
+++ 15. Minimal API/MinimalAPI/Program.cs
@@ -1,26 +1,43 @@
 using MinimalAPI.Models;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<Product> products = new List<Product>()
-{
+List<Product> products = new List<Product>() { 
     new Product() { Id = 1, ProductName = "Smart Phone" },
     new Product() { Id = 2, ProductName = "Smart TV" }
 };
 
-// Get /products
-app.MapGet("/products", async (HttpContext context) =>
-{
-    var content = string.Join('\n', products.Select(temp => temp.ToString()));
+//GET /products
+app.MapGet("/products", async (HttpContext context) => {
+    // Sample Response:
+    // [{ "Id": 1, "ProductName": "Smart Phone" }, { "Id": 2, "ProductName": "Smart TV" }]
 
-    await context.Response.WriteAsync(content);
+    await context.Response.WriteAsync(JsonSerializer.Serialize(products));
 });
 
-app.MapPost("/products", async (HttpContext context, Product product) =>
+
+// GET /products/{id}
+app.MapGet("/products/{id:int}", async (HttpContext context, int id) =>
 {
+    Product? product = products.FirstOrDefault(temp => temp.Id == id);
+    if (product == null)
+    {
+        context.Response.StatusCode = 400; //Bad Request
+        await context.Response.WriteAsync("Incorrect Product ID");
+        return;
+    }
+
+    await context.Response.WriteAsync(JsonSerializer.Serialize(product));
+});
+
+
+// POST /products
+app.MapPost("/products", async (HttpContext context, Product product) => {
     products.Add(product);
     await context.Response.WriteAsync("Product Added");
 });
+
 
 app.Run();
