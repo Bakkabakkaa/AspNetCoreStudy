a80c6e95d78d034d94cd38801a1fded8c919b1f6
Art'm <kasko.artem45@gmail.com>
2026-03-11 21:07:44 +0300

    Get and Post with Minimal API

--- 15. Minimal API/MinimalAPI/Program.cs
+++ 15. Minimal API/MinimalAPI/Program.cs
@@ -1,15 +1,26 @@
+using MinimalAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Get
-app.MapGet("/", async (HttpContext context) =>
+List<Product> products = new List<Product>()
 {
-    await context.Response.WriteAsync("GET - Hello World");
+    new Product() { Id = 1, ProductName = "Smart Phone" },
+    new Product() { Id = 2, ProductName = "Smart TV" }
+};
+
+// Get /products
+app.MapGet("/products", async (HttpContext context) =>
+{
+    var content = string.Join('\n', products.Select(temp => temp.ToString()));
+
+    await context.Response.WriteAsync(content);
 });
 
-app.MapPost("/", async (HttpContext context) =>
+app.MapPost("/products", async (HttpContext context, Product product) =>
 {
-    await context.Response.WriteAsync("GET - Hello World");
+    products.Add(product);
+    await context.Response.WriteAsync("Product Added");
 });
 
 app.Run();
